441fc8ec4db44c2c13b4646abc63c772590a0b18
vlcp197 <vlcp197@gmail.com>
2022-04-15 02:10:44 -0300

    Adicionado funções de alteração e exclusão de dados

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from core.models import Evento
+# Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo','data_evento','data_criacao','local_evento')
+    list_filter = ('usuario','data_evento','local_evento')
+admin.site.register(Evento,EventoAdmin)
---
+++ core/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import  datetime
+# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100,
+                              verbose_name='Título')
+    descricao = models.TextField(blank=True,
+                                 null=True,
+                                 verbose_name='Descrição')
+    data_evento = models.DateTimeField(verbose_name=
+                                       'Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True,
+                                        verbose_name='Data da Criação')
+    local_evento = models.CharField(max_length=100,
+                                    verbose_name='Local',
+                                    blank=True,null=True)
+    usuario = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y - %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
